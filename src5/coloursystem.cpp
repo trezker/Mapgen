f5337720a88af02be1eda92ca4a91b9d7687d7c2
Anders Andersson <trezker@gmail.com>
2011-08-21 10:41:26 +0200

    Build coloursystem module

---
+++ src5/coloursystem.cpp
@@ -0,0 +1,138 @@
+#include "coloursystem.h"
+
+Colourpoint::Colourpoint()
+:p(0)
+,r(0)
+,g(0)
+,b(0)
+{
+}
+
+void Colourpoint::Set_position(float ip)
+{
+	p = ip;
+}
+
+void Colourpoint::Set_colour(int ir, int ig, int ib)
+{
+	r = ir/256.f;
+	g = ig/256.f;
+	b = ib/256.f;
+}
+
+void Colourpoint::Set_colour_f(float ir, float ig, float ib)
+{
+	r = ir;
+	g = ig;
+	b = ib;
+}
+
+float Colourpoint::Get_r() const
+{
+	return r;
+}
+
+float Colourpoint::Get_g() const
+{
+	return g;
+}
+
+float Colourpoint::Get_b() const
+{
+	return b;
+}
+
+float Colourpoint::Get_p() const
+{
+	return p;
+}
+
+Colourpoint Colourpoint::Interpolate(const Colourpoint& c, float pin) const
+{
+	float nr = (c.r - r)/(c.p-p)*(pin-p) + r;
+	float ng = (c.g - g)/(c.p-p)*(pin-p) + g;
+	float nb = (c.b - b)/(c.p-p)*(pin-p) + b;
+//	return int((c2 - c1)/(p2 - p1)*(pin - p1) + c1);
+	Colourpoint n;
+	n.Set_colour(nr, ng, nb);
+	n.Set_position(pin);
+	return n;
+}
+
+
+void Coloursystem::Add_colourpoint(const Colourpoint& c)
+{
+	for(Colourpoints::iterator i=colourpoints.begin(); i!= colourpoints.end(); ++i) {
+		if(i->Get_p() > c.Get_p()) {
+			colourpoints.insert(i, c);
+			return;
+		}
+	}
+	colourpoints.push_back(c);
+}
+
+Colourpoint& Coloursystem::Get_colourpoint(float pin)
+{
+	Colourpoints::iterator j = colourpoints.begin();
+	for(Colourpoints::iterator i=colourpoints.begin(); i!= colourpoints.end(); ++i) {
+		if(i->Get_p() > pin) {
+			if(i->Get_p() - pin < pin - j->Get_p())
+			{
+				return *i;
+			} else {
+				return *j;
+			}
+		}
+		++j;
+	}
+	return colourpoints.back();
+}
+
+const Colourpoint& Coloursystem::Get_colourpoint(float pin) const
+{
+	Colourpoints::const_iterator j = colourpoints.begin();
+	for(Colourpoints::const_iterator i=colourpoints.begin(); i!= colourpoints.end(); ++i) {
+		if(i->Get_p() > pin) {
+			if(i->Get_p() - pin < pin - j->Get_p())
+			{
+				return *i;
+			} else {
+				return *j;
+			}
+		}
+		++j;
+	}
+	return colourpoints.back();
+}
+
+void Coloursystem::Remove_colourpoint(float pin)
+{
+	Colourpoints::iterator j = colourpoints.begin();
+	for(Colourpoints::iterator i=colourpoints.begin(); i!= colourpoints.end(); ++i) {
+		if(i->Get_p() > pin) {
+			if(i->Get_p() - pin < pin - j->Get_p())
+			{
+				colourpoints.erase(i);
+				return;
+			} else {
+				colourpoints.erase(j);
+				return;
+			}
+		}
+		++j;
+	}
+	colourpoints.pop_back();
+}
+
+Colourpoint Coloursystem::Interpolate(float pin) const
+{
+	Colourpoints::const_iterator j = colourpoints.begin();
+	for(Colourpoints::const_iterator i=colourpoints.begin(); i!= colourpoints.end(); ++i) {
+		if(i->Get_p() > pin) {
+			return i->Interpolate(*j, pin);
+		}
+		++j;
+	}
+	Colourpoint n = colourpoints.back();
+	return n;
+}
---
+++ src5/coloursystem.h
@@ -0,0 +1,36 @@
+#ifndef mapgen_coloursystem_h
+#define mapgen_coloursystem_h
+
+#include <vector>
+
+class Colourpoint {
+public:
+	Colourpoint();
+	void Set_position(float p);
+	void Set_colour(int r, int g, int b);
+	void Set_colour_f(float r, float g, float b);
+	float Get_r() const;
+	float Get_g() const;
+	float Get_b() const;
+	float Get_p() const;
+	Colourpoint Interpolate(const Colourpoint& c, float pin) const;
+private:
+	float p;
+	float r;
+	float g;
+	float b;
+};
+
+class Coloursystem {
+public:
+	void Add_colourpoint(const Colourpoint& c);
+	Colourpoint& Get_colourpoint(float pin);
+	const Colourpoint& Get_colourpoint(float pin) const;
+	void Remove_colourpoint(float pin);
+	Colourpoint Interpolate(float pin) const;
+private:
+	typedef std::vector<Colourpoint> Colourpoints;
+	Colourpoints colourpoints;
+};
+
+#endif
